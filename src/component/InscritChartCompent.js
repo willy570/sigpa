736f7f9a30f5e339bb87ca859508d69ebfe970b2
Kamelan <willy570@users.noreply.github.com>
2022-06-01 02:52:19 +0000

    ajout graph inscription

---
+++ src/component/InscritChartCompent.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  AreaChart,
+  Area
+} from "recharts";
+
+
+const InscritChartComponent = (props) => {
+  const { datas } = props;
+  const styles = {
+    marginTop: "6em",
+  };
+
+  if (!datas || datas.length === 0)
+    return <p>No data found, sorry</p>;
+  return (
+  
+    <BarChart width={600} height={400} data={datas} style={styles}>
+    <XAxis dataKey="date" height={70} />
+    <YAxis
+      width={100}
+      height={25}
+      allowDecimals="true"
+      allowDuplicatedCategory="false"
+      tickCount={9}
+    />
+    <Legend />
+    <CartesianGrid stroke="#eee" strokeDasharray="3 3" />
+    <Bar
+      type="monotone"
+      barSize="25"
+      dataKey="notesymbole"
+      fill="#0c53ac"
+    />
+    <Bar
+      type="monotone"
+      barSize="25"
+      dataKey="notenumeratie"
+      fill="#07aaf1"
+    />
+  
+    <Tooltip type="" />
+  </BarChart>
+
+  );
+};
+
+export default InscritChartComponent;
--- src/home/Home.js
+++ src/home/Home.js
@@ -4,25 +4,55 @@ import axios from "axios";
 import LoadingTable from "../component/LoadingTable";
 import Mtable from "../component/Mtable";
 import ChartComponent from "../component/ChartCompent";
+import InscritChartComponent from '../component/InscritChartCompent';
 
 export const Home = () => {
     const apiUrl = "https://api.sigpa.org/alpha.php";
+    const inscritUrl = "https://api.sigpa.org/alpha.php/inscrit.php";
     const TableLoading = LoadingTable(Mtable);
     const [appState, setAppState] = useState({
       loading: false,
       datas: null,
+      inscritdata:null
     });
+
+    useEffect(() =>{
+      setAppState({ loading: true });
+      Promise.all([
+        axios.get(apiUrl),
+        axios.get(inscritUrl),
+      ]).then((datas) =>{
+        console.log(datas[0]);
+        console.log(datas[1]);
+        const allData = datas[0].data;
+         const inscrits = datas[1].data;
+        setAppState({ loading: false,datas: allData, inscritdata: inscrits });
+       
+      }).catch(function (error) {
+       
+        console.log(error);
+    });
+      
+    },[setAppState])
   
-    useEffect(() => {
+    /* useEffect(() => {
       setAppState({ loading: true });
       axios.get(apiUrl).then((datas) => {
         const allData = datas.data;
         setAppState({ loading: false, datas: allData });
       });
+
     }, [setAppState]);
+    useEffect(() =>{
+        setAppState({ loading: true });
+      axios.get(inscritUrl).then((datas) => {
+        const allData = datas.data;
+        setAppState({ loading: false, datas: allData });
+      });
+    }, [setAppState]) */
   return (
     <div> 
-        <div className="bg-gray-50 h-auto">
+        <div className="bg-gray-50 h-auto ml-5 mr-5">
           <div className="containter px-7 bg-white mx-auto">
             <div className="grid grid-cols-3 my-8">
               <div className="w-3/12"></div>
@@ -43,6 +73,18 @@ export const Home = () => {
               <div className="shadow">
                 <ChartComponent chartDatas={appState.datas}/>
               </div>
+            </div>
+            <div className="m-5">
+                <h1 className="text-3xl subpixel-antialiased font-semibold font-nono underline decoration-pink-500">Nombre d'inscrit</h1>
+            </div>
+            <div className="grid grid-cols-1  mb-5">
+               
+          
+              <div className="shadow">
+                  <InscritChartComponent datas={appState.inscritdata}/>
+              
+              </div>
+             
             </div>
           </div>
         </div>
